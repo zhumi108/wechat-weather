9111acadbe866a07419aa9f81ace970c70fd3867
Jeremy <>
2018-06-03 10:46:18 +0800

    优达学城天气查询小程序

---
+++ pages/index/index.js
@@ -0,0 +1,60 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBackground: '',
+    forecast: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  },
+  onLoad() {
+    this.getNow()
+  },
+  onPullDownRefresh() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        'city': '南京市'
+      },
+      success: res => {
+        console.log(res.data);
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        this.setData({
+          nowTemp: temp + "˚",
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
+        });
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  }
+})
